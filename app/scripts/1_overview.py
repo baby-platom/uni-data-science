03a89f372803ab85e657aa66c3cf35c093b6920d
baby-platom <platon.programmer@gmail.com>
2025-12-07 14:48:26 +0100

    refactor(1_overview): better script structure

--- app/scripts/1_overview.py
+++ app/scripts/1_overview.py
@@ -4,13 +4,6 @@ import pandas as pd
 
 from app.constants import TIME_SERIES_CSV_PATH, WEATHER_CSV_PATH
 
-time_series_df = pd.read_csv(TIME_SERIES_CSV_PATH)
-weather_df = pd.read_csv(WEATHER_CSV_PATH)
-
-# 0. Basic
-
-print("\n---Basic---\n")
-
 
 def get_basic_overview(df: pd.DataFrame, df_name: str, top_columns_n: int = 25) -> None:
     print(f"\n{df_name} overview:")
@@ -20,18 +13,9 @@ def get_basic_overview(df: pd.DataFrame, df_name: str, top_columns_n: int = 25) 
     print(list(df.columns)[:top_columns_n])
 
 
-get_basic_overview(time_series_df, TIME_SERIES_CSV_PATH.name)
-get_basic_overview(weather_df, WEATHER_CSV_PATH.name)
-
-
-# 1. Check country codes
-
-print("\n---Country codes---\n")
-
-pattern = re.compile(r"^[A-Z]{2}_")
-
-
 def get_country_codes(df: pd.DataFrame) -> set[str]:
+    pattern = re.compile(r"^[A-Z]{2}_")
+
     result = set()
 
     for column in df.columns:
@@ -42,39 +26,10 @@ def get_country_codes(df: pd.DataFrame) -> set[str]:
     return result
 
 
-time_series_country_codes = get_country_codes(time_series_df)
-weather_country_codes = get_country_codes(weather_df)
-
-print("Country codes N")
-print(f"'{TIME_SERIES_CSV_PATH.name}': {len(time_series_country_codes)}")
-print(f"'{WEATHER_CSV_PATH.name}': {len(weather_country_codes)}")
-
-intersecting_country_codes = time_series_country_codes & weather_country_codes
-print(f"\nIntersecting country codes ({len(intersecting_country_codes)}):")
-print(sorted(intersecting_country_codes))
-
-# 2. Validate timestamps
-
-print("\n---Time stamps---\n")
-
-time_series_df["utc_timestamp"] = pd.to_datetime(
-    time_series_df["utc_timestamp"],
-    utc=True,
-)
-weather_df["utc_timestamp"] = pd.to_datetime(
-    weather_df["utc_timestamp"],
-    utc=True,
-)
-
-
 def describe_utc_timestamp(df: pd.DataFrame, df_name: str) -> None:
     print("\nDescribe `utc_timestamp`:")
     print(f"'{df_name}'")
     print(df["utc_timestamp"].describe())
-
-
-describe_utc_timestamp(time_series_df, TIME_SERIES_CSV_PATH.name)
-describe_utc_timestamp(weather_df, WEATHER_CSV_PATH.name)
 
 
 def validate_utc_timestamp(df: pd.DataFrame, df_name: str) -> None:
@@ -100,5 +55,41 @@ def validate_utc_timestamp(df: pd.DataFrame, df_name: str) -> None:
     print("Unexpected (extra) timestamps:", list(extra_timestamps))
 
 
-validate_utc_timestamp(time_series_df, TIME_SERIES_CSV_PATH.name)
-validate_utc_timestamp(weather_df, WEATHER_CSV_PATH.name)
+if __name__ == "__main__":
+    time_series_df = pd.read_csv(TIME_SERIES_CSV_PATH)
+    weather_df = pd.read_csv(WEATHER_CSV_PATH)
+
+    # 0. Basic
+    print("\n---Basic---\n")
+    get_basic_overview(time_series_df, TIME_SERIES_CSV_PATH.name)
+    get_basic_overview(weather_df, WEATHER_CSV_PATH.name)
+
+    # 1. Check country codes
+    print("\n---Country codes---\n")
+    time_series_country_codes = get_country_codes(time_series_df)
+    weather_country_codes = get_country_codes(weather_df)
+
+    print("Country codes N")
+    print(f"'{TIME_SERIES_CSV_PATH.name}': {len(time_series_country_codes)}")
+    print(f"'{WEATHER_CSV_PATH.name}': {len(weather_country_codes)}")
+
+    intersecting_country_codes = time_series_country_codes & weather_country_codes
+    print(f"\nIntersecting country codes ({len(intersecting_country_codes)}):")
+    print(sorted(intersecting_country_codes))
+
+    # 2. Validate timestamps
+    print("\n---Time stamps---\n")
+    time_series_df["utc_timestamp"] = pd.to_datetime(
+        time_series_df["utc_timestamp"],
+        utc=True,
+    )
+    weather_df["utc_timestamp"] = pd.to_datetime(
+        weather_df["utc_timestamp"],
+        utc=True,
+    )
+
+    describe_utc_timestamp(time_series_df, TIME_SERIES_CSV_PATH.name)
+    describe_utc_timestamp(weather_df, WEATHER_CSV_PATH.name)
+
+    validate_utc_timestamp(time_series_df, TIME_SERIES_CSV_PATH.name)
+    validate_utc_timestamp(weather_df, WEATHER_CSV_PATH.name)
